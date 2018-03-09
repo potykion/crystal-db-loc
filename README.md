59a5d2fe603579498d1e4e50117226e3c6505b8e
potykion <potykion@gmail.com>
2018-03-09 17:45:30 +0300

    Triggers disabling.

---
+++ README.md
@@ -0,0 +1,3 @@
+# References
+
+1. [SQL Server Disabling All Triggers](https://stackoverflow.com/questions/7176720/sql-server-disabling-all-triggers-cannot-find-the-object-xxxx-because-it-doe)
--- main.py
+++ main.py
@@ -14,3 +14,23 @@ if __name__ == '__main__':
                 print(localize_only_ru_columns_table(table), file=f)
             elif type_ == 'HAS_RU_COLUMNS':
                 print(localize_has_ru_columns_table(table), file=f)
+
+        print('ALTER TABLE DensTablLanguage ADD PRIMARY KEY (ID);', file=f)
+        print('''
+        -- Создаем таблицу для возможных языков
+        CREATE TABLE dbo.Languages (
+            ID int NOT NULL PRIMARY KEY IDENTITY(1,1),
+            Name varchar(2)
+        );
+        
+        -- Создаем языки
+        INSERT INTO dbo.Languages 
+        (Name)
+        VALUES
+        ('ru'),  ('en');
+        GO
+        
+        -- Отключаем триггеры        
+        sp_msforeachtable 'ALTER TABLE ? DISABLE TRIGGER all'
+        GO
+        ''')
