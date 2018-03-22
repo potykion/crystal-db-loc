2493d26ee56e915d6fc71b6a9626d0df45e97525
potykion <potykion@gmail.com>
2018-03-22 12:51:59 +0300

    Connect to db method

--- crystal/db.py
+++ crystal/db.py
@@ -2,11 +2,14 @@ from urllib.parse import quote_plus
 
 from records import Database
 
-database = 'Crystal'
-driver = 'SQL Server'
-server = '.\SQLEXPRESS'
 
-params = quote_plus(f'DRIVER={driver};SERVER={server};DATABASE={database};')
-database_url = f'mssql+pyodbc:///?odbc_connect={params}'
+def _connect_to_db(database='Crystal'):
+    driver = 'SQL Server'
+    server = '.\SQLEXPRESS'
 
-db = Database(database_url)
+    params = quote_plus(f'DRIVER={driver};SERVER={server};DATABASE={database};')
+    database_url = f'mssql+pyodbc:///?odbc_connect={params}'
+    return Database(database_url)
+
+
+db = _connect_to_db()
