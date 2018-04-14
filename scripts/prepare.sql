08063f43dee9e1f26ff9c6387c8f6bbb67638033
potykion <potykion@gmail.com>
2018-04-15 00:40:30 +0300

    Add PK to RefrTabl

--- scripts/prepare.sql
+++ scripts/prepare.sql
@@ -8,3 +8,17 @@ GO
 ---Создаем PK для DensTabl
 ALTER TABLE dbo.DensTabl ADD PRIMARY KEY (ID);
 GO
+
+---Создаем PK для RefrTabl
+alter table [dbo].[RefrTabl]
+drop constraint [PK_RefrTabl]
+GO
+
+ALTER TABLE [dbo].[RefrTabl]
+ADD ID INT IDENTITY(1,1)
+GO
+
+ALTER TABLE [dbo].[RefrTabl]
+ADD CONSTRAINT [PK_RefrTabl]
+PRIMARY KEY CLUSTERED (ID)
+GO
