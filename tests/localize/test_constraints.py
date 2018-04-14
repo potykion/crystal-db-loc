2a645c89a6d9c1d66bd81f55b0f3e96ae000c261
potykion <potykion@gmail.com>
2018-04-14 19:08:18 +0300

    Remove delete constraints scripts

--- tests/localize/test_constraints.py
+++
@@ -1,8 +0,0 @@
-from crystal.tables.dependencies import list_default_constraints
-
-
-def test_list_default_constraints():
-    constraints = list_default_constraints('HeadTabl')
-    constraint_columns = set(constraint['column_name'] for constraint in constraints)
-    assert {'System', 'Expert', 'Help', 'Class'} == constraint_columns
-
