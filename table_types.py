587b5344d2331d931c98b5b41a78690808f8a82e
potykion <potykion@gmail.com>
2018-05-05 16:04:20 +0300

    Find table types and tables without pks.

---
+++ table_types.py
@@ -0,0 +1,133 @@
+import json
+from collections import defaultdict
+from operator import itemgetter
+from typing import Iterable
+
+from records import Record
+
+from utils import db
+
+
+def find_tables_fks(tables):
+    table_fks = defaultdict(list)
+
+    for table in tables:
+        fks = db.query(f'sp_fkeys {table}')
+
+        for fk in fks:
+            fk_table = fk['FKTABLE_NAME']
+            fk_column = fk['FKCOLUMN_NAME']
+            table_fks[fk_table].append(fk_column)
+
+    return table_fks
+
+
+def find_tables_pks(tables):
+    return dict(zip(
+        tables, map(find_table_pks, tables)
+    ))
+
+
+def find_table_pks(table):
+    return [
+        pk['COLUMN_NAME']
+        for pk in db.query(f'sp_pkeys {table}').all()
+    ]
+
+
+def find_tables_without_pks(tables, table_pks):
+    return [
+        table
+        for table in tables
+        if not table_pks[table] or
+           'HeadClue' in table_pks[table] and table not in ['SingTabl', 'HeadTabl']
+    ]
+
+
+def find_tables_types(tables, table_pks, table_fks):
+    return dict(
+        sorted(
+            (
+                (table, identify_table_type(table, table_pks[table], table_fks[table]))
+                for table in tables
+            ),
+            key=itemgetter(1)
+        )
+    )
+
+
+def identify_table_type(table, fks, pks):
+    columns = get_columns(table)
+    columns_without_fk_and_pk = [
+        column
+        for column in columns
+        if column not in pks and
+           column not in fks
+    ]
+
+    ru_columns = find_ru_columns(table)
+
+    columns_count = len(columns_without_fk_and_pk)
+    ru_columns_count = len(ru_columns)
+
+    if ru_columns_count and columns_count == ru_columns_count:
+        return 'ONLY_RU_COLUMNS'
+    elif ru_columns_count and columns_count != ru_columns_count:
+        return 'HAS_RU_COLUMNS'
+    else:
+        return 'NO_RU_COLUMNS'
+
+
+def get_columns(table):
+    return [
+        column['COLUMN_NAME']
+        for column in db.query(f'sp_columns {table}')
+    ]
+
+
+def find_ru_columns(table):
+    rows = get_rows(table)
+    columns = set()
+
+    for row in rows:
+        row_dict = row.as_dict()
+
+        if len(columns) == len(row_dict):
+            break
+
+        for column, value in row_dict.items():
+            if column in columns:
+                continue
+
+            if is_ru(value):
+                columns.add(column)
+
+    return columns
+
+
+def get_rows(table) -> Iterable[Record]:
+    return db.query(f'SELECT * FROM {table}')
+
+
+ru_alphabet = 'йцукенгшщзхъфывапролджэячсмитьбюё'
+
+
+def is_ru(value):
+    return any(char_ in ru_alphabet for char_ in str(value))
+
+
+if __name__ == '__main__':
+    with open('data/0_tables.json') as f:
+        tables = json.load(f)
+
+    table_fks = find_tables_fks(tables)
+    table_pks = find_tables_pks(tables)
+
+    tables_without_pk = find_tables_without_pks(tables, table_pks)
+    with open('data/1_tables_without_pks.json', 'w') as f:
+        json.dump(tables_without_pk, f)
+
+    tables_with_type = find_tables_types(tables, table_pks, table_fks)
+
+    with open('data/1_tables_with_types.json', 'w') as f:
+        json.dump(tables_with_type, f)
