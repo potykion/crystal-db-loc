bcd3c77e2df4e515368c6f830c35fe987055e6fe
potykion <potykion@gmail.com>
2018-05-17 19:51:55 +0300

    Page context for NO_RU_COLUMNS_tables

--- page_context_generator.py
+++ page_context_generator.py
@@ -1,4 +1,5 @@
 import json
+from functools import partial
 
 from common.db import db
 from common.table import get_columns, find_table_pks, drop_computed_columns
@@ -36,28 +37,18 @@ def get_page_table_columns(table):
     return table_columns_without_pk_and_computed
 
 
-def build_context_for_table(table_with_properties):
+def build_context_for_table(table_with_properties, invariant_with_suffix=True):
     table, properties = table_with_properties
 
-    language_table_columns = get_page_table_columns(f'{table}Language')
-    try_remove(language_table_columns, f'{table}ID')
-    try_remove(language_table_columns, f'LanguageID')
-    try_remove(language_table_columns, 'HeadClue')
+    try:
+        language_table_columns = get_page_table_columns(f'{table}Language')
+        try_remove(language_table_columns, f'{table}ID')
+        try_remove(language_table_columns, f'LanguageID')
+        try_remove(language_table_columns, 'HeadClue')
+    except Exception:
+        language_table_columns = []
 
-    invariant_table_columns = get_page_table_columns(f'{table}Invariant')
-    try_remove(invariant_table_columns, 'HeadClue')
-
-    if 'SingCode' in invariant_table_columns:
-        try_remove(invariant_table_columns, 'SingCode')
-        has_sing = True
-    else:
-        has_sing = False
-
-    if 'Bknumber' in invariant_table_columns:
-        has_references = True
-        try_remove(invariant_table_columns, 'Bknumber')
-    else:
-        has_references = False
+    invariant_table_columns, has_references, has_sing = _extract_invariant_data(table, invariant_with_suffix)
 
     context = {
         'model': table,
@@ -78,31 +69,30 @@ def build_context_for_table(table_with_properties):
     return context
 
 
-if __name__ == '__main__':
-    with open('data/1_tables_with_types.json') as f:
-        tables_with_types = json.load(f)
+def _extract_invariant_data(table, with_suffix=True):
+    invariant_table = table + ('Invariant' if with_suffix else '')
 
-    split_tables = [
-        table
-        for table, type_ in tables_with_types.items()
-        if type_ == 'HAS_RU_COLUMNS' and
-           table not in ['Bibliogr', 'LastModified', 'Properties', 'HeadTabl', 'SingTabl']
-    ]
+    invariant_table_columns = get_page_table_columns(invariant_table)
+    try_remove(invariant_table_columns, 'HeadClue')
 
-    properties = db.query(PROPERTIES_QUERY)
-    table_properties = {
-        property_['TableName']: property_
-        for property_ in properties
-    }
+    if 'SingCode' in invariant_table_columns:
+        try_remove(invariant_table_columns, 'SingCode')
+        has_sing = True
+    else:
+        has_sing = False
+    if 'Bknumber' in invariant_table_columns:
+        has_references = True
+        try_remove(invariant_table_columns, 'Bknumber')
+    else:
+        has_references = False
 
-    # todo no properties for:
-    # ElemTablNew
-    # GrafTabl
-    # MnOpTabl
-    # EquationTabl
-    # todo NO_RU_COLUMNS requires special context
+    return invariant_table_columns, has_references, has_sing
+
+
+def _build_contexts(tables, table_properties, invariant_with_suffix=True):
     tables_with_properties = []
-    for table in split_tables:
+
+    for table in tables:
         if table == 'HeatExpn':
             tables_with_properties.append((table, table_properties['HeatExpn0']))
             tables_with_properties.append((table, table_properties['HeatExpn1']))
@@ -111,7 +101,29 @@ if __name__ == '__main__':
         else:
             print(table)
 
-    contexts = list(map(build_context_for_table, tables_with_properties))
+    context_builder = partial(build_context_for_table, invariant_with_suffix=invariant_with_suffix)
+    contexts = list(map(context_builder, tables_with_properties))
+    return contexts
+
+
+if __name__ == '__main__':
+    with open('data/1_tables_with_types.json') as f:
+        tables_with_types = json.load(f)
+
+    HAS_RU_COLUMNS_tables = [
+        table
+        for table, type_ in tables_with_types.items()
+        if table not in ['Bibliogr', 'LastModified', 'Properties', 'HeadTabl', 'SingTabl']
+           and type_ == 'HAS_RU_COLUMNS'
+    ]
+    table_properties = {
+        property_['TableName']: property_
+        for property_ in db.query(PROPERTIES_QUERY)
+    }
+    contexts = _build_contexts(HAS_RU_COLUMNS_tables, table_properties)
+
+    NO_RU_COLUMNS_tables = {'Wavepure', 'SistTabl', 'LitrTabl'}
+    contexts.extend(_build_contexts(NO_RU_COLUMNS_tables, table_properties, invariant_with_suffix=False))
 
     with open('data/page_contexts.json', 'w', encoding='utf-8') as f:
         json.dump(contexts, f)
